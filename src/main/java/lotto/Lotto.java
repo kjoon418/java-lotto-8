2004af7915a8e2ca3cdd5ad8b02c5a14cfb76c29
Kim, Joon <kjoon418@naver.com>
2025-10-30 14:43:53 +0900

    chore(model): 로또 번호의 범위 검증 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,18 +3,35 @@ package lotto;
 import java.util.List;
 
 public class Lotto {
+    private static final int SIZE = 6;
+    private static final int MINIMUM = 1;
+    private static final int MAXIMUM = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateRange(numbers);
+
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != SIZE) {
+            throw new IllegalArgumentException("로또 번호는 " + SIZE + "개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        boolean outOfRange = numbers.stream()
+                .anyMatch(this::isOutOfRange);
+
+        if (outOfRange) {
+            throw new IllegalArgumentException("로또 번호는 [" + MINIMUM + ", " + MAXIMUM + "] 범위에 속해야 합니다.");
+        }
+    }
+
+    private boolean isOutOfRange(Integer number) {
+        return number < MINIMUM || number > MAXIMUM;
+    }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,12 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoTest {
     @Test
@@ -14,12 +15,23 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -100, Integer.MIN_VALUE})
+    void 로또_번호가_1보다_작으면_예외가_발생한다(int illegalNumber) {
+        assertThatThrownBy(() -> new Lotto(List.of(illegalNumber, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {46, 500, Integer.MAX_VALUE})
+    void 로또_번호가_45보다_크면_예외가_발생한다(int illegalNumber) {
+        assertThatThrownBy(() -> new Lotto(List.of(illegalNumber, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
