f8a648abefce52ac3e32448ed4c8b6d4cdc0ad7a
Kim, Joon <kjoon418@naver.com>
2025-10-30 14:55:23 +0900

    refactor(model): `Lotto`의 패키지 경로 수정

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -1,9 +1,8 @@
-package lotto;
+package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
