49d5f403054c14b93f764a377be1f9648b81b4d7
Kim, Joon <kjoon418@naver.com>
2025-10-30 13:24:34 +0900

    feat(view): 발행한 로또 출력 기능 구현

--- src/main/java/lotto/view/LottoOutputView.java
+++ src/main/java/lotto/view/LottoOutputView.java
@@ -1,7 +1,12 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.dto.LottoDto;
+
 public interface LottoOutputView {
     void printPurchaseAmountGuide();
+
+    void printPurchasedLottos(List<LottoDto> lottos);
 
     void printEmptyLine();
 }
--- src/main/java/lotto/view/LottoOutputViewImpl.java
+++ src/main/java/lotto/view/LottoOutputViewImpl.java
@@ -1,13 +1,40 @@
 package lotto.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.dto.LottoDto;
+
 public class LottoOutputViewImpl implements LottoOutputView {
+    private static final String LOTTO_PREFIX = "[";
+    private static final String LOTTO_SUFFIX = "]";
+    private static final String LOTTO_DELIMITER = ", ";
+
     @Override
     public void printPurchaseAmountGuide() {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
     @Override
+    public void printPurchasedLottos(List<LottoDto> lottos) {
+        System.out.printf("%d개를 구매했습니다.", lottos.size());
+
+        for (LottoDto lotto : lottos) {
+            printLotto(lotto);
+        }
+
+        printEmptyLine();
+    }
+
+    @Override
     public void printEmptyLine() {
         System.out.println();
+    }
+
+    private void printLotto(LottoDto lotto) {
+        String joinedNumbers = lotto.numbers().stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(LOTTO_DELIMITER));
+
+        System.out.println(LOTTO_PREFIX + joinedNumbers + LOTTO_SUFFIX);
     }
 }
