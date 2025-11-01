dd77c35e5aab701758fd18e1b809953fc171ca54
Kim, Joon <kjoon418@naver.com>
2025-11-01 14:09:35 +0900

    chore(dto): `LottoDto`에 정적 팩터리 메서드 추가

--- src/main/java/lotto/dto/LottoDto.java
+++ src/main/java/lotto/dto/LottoDto.java
@@ -1,8 +1,14 @@
 package lotto.dto;
 
 import java.util.List;
+import lotto.model.Lotto;
 
 public record LottoDto(
         List<Integer> numbers
 ) {
+    public LottoDto from(Lotto lotto) {
+        List<Integer> numbers = lotto.getNumbers();
+
+        return new LottoDto(numbers);
+    }
 }
