32b3b5954f6410f9fc4adf983e27e3a68d1eaeae
Kim, Joon <kjoon418@naver.com>
2025-11-01 13:28:14 +0900

    chore(model): `LottoResultCalculator`가 `List<Lotto>`를 파라미터로 받을 수 있도록 메서드 추가

--- src/main/java/lotto/model/LottoResultCalculator.java
+++ src/main/java/lotto/model/LottoResultCalculator.java
@@ -1,5 +1,9 @@
 package lotto.model;
 
+import java.util.List;
+
 public interface LottoResultCalculator {
+    List<LottoResult> calculate(List<Lotto> lottos, WinningLotto winningLotto);
+
     LottoResult calculate(Lotto lotto, WinningLotto winningLotto);
 }
--- src/main/java/lotto/model/LottoResultCalculatorImpl.java
+++ src/main/java/lotto/model/LottoResultCalculatorImpl.java
@@ -1,6 +1,15 @@
 package lotto.model;
 
+import java.util.List;
+
 public class LottoResultCalculatorImpl implements LottoResultCalculator {
+    @Override
+    public List<LottoResult> calculate(List<Lotto> lottos, WinningLotto winningLotto) {
+        return lottos.stream()
+                .map(lotto -> calculate(lotto, winningLotto))
+                .toList();
+    }
+
     @Override
     public LottoResult calculate(Lotto lotto, WinningLotto winningLotto) {
         int equalCount = lotto.getEqualCount(winningLotto);
