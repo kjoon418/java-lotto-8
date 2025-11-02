595ec0dd38f55edacb75ce6518441602a1ad4b6e
Kim, Joon <kjoon418@naver.com>
2025-11-02 11:41:41 +0900

    feat: Application 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.config.LottoConfig;
+import lotto.controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoConfig config = new LottoConfig();
+        LottoController controller = config.lottoController();
+
+        controller.run();
     }
 }
